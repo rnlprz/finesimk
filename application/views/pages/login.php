05d85eaa9d40926ad56b8df62ebe585fb5b2f6a4
rnlprz <rnlprzp@gmail.com>
2018-09-28 05:58:49 -0500

    FINESIMK Version 1.0.0

---
+++ application/views/pages/login.php
@@ -0,0 +1,47 @@
+<div class="container">
+    <br>
+    <div class="row">
+        <div class="col-8 offset-2 text-center">
+            <h4 class="text-info">¡BIENVENIDO USUARIO!</h4>
+            <p class="alert alert-dark">
+                Mediante este sistema usted puede gestionar la cuenta de servicio de internet inalambrico.<span
+                        class="badge badge-warning">Nuevo!</span>
+            </p>
+
+
+        </div>
+        <div class="col-4 offset-4 text-center">
+            <div class="text-center">
+                <div class="search-box">
+                    <div class="caption">
+                        <h3>Advance Password Validation</h3>
+                    </div>
+                    <form action="" class="loginForm">
+                        <div class="input-group">
+                            <input type="text" id="name" class="form-control" placeholder="Full Name">
+                            <input type="password" id="paw" class="form-control" placeholder="Password">
+                            <input type="submit" id="submit" class="form-control" value="Submit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="">
+                <div class="aro-pswd_info">
+                    <div id="pswd_info">
+                        <h4>Password must be requirements</h4>
+                        <ul>
+                            <li id="letter" class="invalid">At least <strong>one letter</strong></li>
+                            <li id="capital" class="invalid">At least <strong>one capital letter</strong></li>
+                            <li id="number" class="invalid">At least <strong>one number</strong></li>
+                            <li id="length" class="invalid">Be at least <strong>8 characters</strong></li>
+                            <li id="space" class="invalid">be<strong> use [~,!,@,#,$,%,^,&,*,-,=,.,;,']</strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            <div/>
+
+        </div>
+    </div>
---
+++ application/views/pages/portal.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dayk
+ * Date: 28/07/18
+ * Time: 01:56 PM
+ */
